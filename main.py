97555d86209747aa4acf46bbbbabe4de4e07c141
Raykesh <raykesh.ravirooban@reseau.eseo.>
2025-09-25 10:31:08 +0200

    On branch Dev-Raykesh Your branch is up to date with 'origin/Dev-Raykesh'.
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Tout fonctionne !")
+
+
+def main():
+    print("Bienvenue dans le jeu Roguelike !")
+    # Initialisation du jeu et boucle principale ici
+    print("Tout fonctionne !")
+
+if __name__ == "__main__":
+    main()
