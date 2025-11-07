0180cc4bdd5028d317eee7193d76ea5a1f6e740a
Raykesh <raykesh.ravirooban@reseau.eseo.>
2025-11-07 15:10:02 +0100

    On branch Dev-Raykesh Your branch is up to date with 'origin/Dev-Raykesh'.
    Changes to be committed:
    	modified:   logs/app.jsonl
    	modified:   main.py
    
    Changes not staged for commit:
    	deleted:    logs/episodes/2025-11-07/episode_06031cd4-ae5a-4cd4-929b-f85b67b254b1.jsonl
    	deleted:    logs/episodes/2025-11-07/episode_97d727d1-9a6e-4361-8b5c-c5375f43d4e7.jsonl
    	deleted:    logs/episodes/2025-11-07/episode_b318b617-a12d-49eb-9b1c-9ea5c480ebe3.jsonl
    
    Untracked files:
    	logs/episodes/2025-11-07/episode_9cb06cd2-3246-43b4-8388-9a886825ac7f.jsonl
    	logs/episodes/2025-11-07/episode_d071d328-bd4c-4e2a-a4f2-c71b29ded575.jsonl

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from engine.game import run_game
 from system.logging_setup import setup_json_logging
-
+from dotenv import load_dotenv
+import os
 
 def main():
     """ Lance le jeu """
@@ -10,5 +11,7 @@ def main():
     run_game()
 
 if __name__ == "__main__":
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.', 'database', '.env')
+    load_dotenv(dotenv_path=dotenv_path)
     main()
     
