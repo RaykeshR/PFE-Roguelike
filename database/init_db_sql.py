e68022602a550046acc115f837919d472fb81be0
Sabri <sabri.messaoudi@reseau.eseo.fr>
2025-11-11 19:33:56 +0100

    connection user & save q_table par joueur

--- database/init_db_sql.py
+++ database/init_db_sql.py
@@ -9,7 +9,7 @@ CREATE TABLE utilisateurs (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100),
-    mdp VARCHAR(50),
+    mdp VARCHAR(100),
     created_at TIMESTAMP DEFAULT NOW()
 );
 
