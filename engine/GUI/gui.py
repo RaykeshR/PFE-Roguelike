24a928ff3cc528c6374472d67d7d22a86c733d0c
Raykesh <raykesh.ravirooban@reseau.eseo.>
2026-01-08 11:00:20 +0100

    fix gui On branch Dev-Raykesh Your branch is up to date with 'origin/Dev-Raykesh'.
    Changes to be committed:
    	modified:   engine/GUI/gui.py

--- engine/GUI/gui.py
+++ engine/GUI/gui.py
@@ -398,9 +398,6 @@ class GameGUI:
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(title)
-<<<<<<< HEAD
-=======
-
         # Set window icon
         icon_path = resource_path("src/gameplay.ico")
         try:
@@ -408,7 +405,6 @@ class GameGUI:
             pygame.display.set_icon(program_Icon)
         except Exception as e:
             print(f"Error loading icon: {e}")
->>>>>>> e1ee9e50f82e4cfb06348ea23bc5b69ec02b4e18
         self.bg_color = bg_color
         self.clock = pygame.time.Clock()
         self.fps = fps
